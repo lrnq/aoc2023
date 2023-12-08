8f1d816f99f2ba29987b81af939a5657028bcce0
lrnq <26392224+lrnq@users.noreply.github.com>
2023-12-08 11:14:39 +0100

    fix

--- 8.py
+++ 8.py
@@ -10,26 +10,30 @@ g = defaultdict(list)
 for line in lines[2:]:
     data = line.replace("(", "").replace(")", "").split("=")
     src = data[0].strip()
-    neighbours = data[1].split(",")
-    for n in neighbours:
+    for n in data[1].split(","):
         g[src].append(n.strip())
 
-for part in [1, 2]:
-    starting_nodes = [x for x in g if x[-1] == "A"] if part == 2 else ["AAA"]
-    starting_nodes_steps = [0]*len(starting_nodes)
-    for i, cur_node in enumerate(starting_nodes):
-        steps = -1
-        for op in cycle(seq):
-            steps += 1
-            if cur_node[-1] == "Z" and part == 2 or cur_node == "ZZZ" and part == 1: 
-                break
-            if op == "L":
-                cur_node = g[cur_node][0]
-            elif op == "R":
-                cur_node = g[cur_node][1]
-        
-        starting_nodes_steps[i] = steps
+def p(v, cond):
+    steps = -1
+    cur = v
+    for op in cycle(seq):
+        steps += 1
+        if cond(cur):
+            break
+        if op == "L":
+            cur = g[cur][0]
+        elif op == "R":
+            cur = g[cur][1]
+    return steps
 
-    print(f"Part {part}:",math.lcm(*starting_nodes_steps))
+
+
+for part in [0, 1]:
+    starting_nodes = [x for x in g if x[-1] == "A"] if part else ["AAA"]
+    starting_nodes_steps = [0]*len(starting_nodes)
+    for i, v in enumerate(starting_nodes):
+        cond = (lambda x: x=="ZZZ") if not part else (lambda x: x[-1] == "Z")
+        starting_nodes_steps[i] = p(v, cond)
+    print(f"Part {part+1}:",math.lcm(*starting_nodes_steps))
     
 
