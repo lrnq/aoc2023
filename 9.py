2dd356b02e36953d43d35dcf2c1c87935225454d
lrnq <26392224+lrnq@users.noreply.github.com>
2023-12-09 15:16:50 +0100

    add day 9

---
+++ 9.py
@@ -0,0 +1,13 @@
+from aocd import get_data
+
+
+def f(s):
+    rows = [s]
+    while any(rows[-1]):
+        rows.append([rows[-1][i] - rows[-1][i - 1] for i in range(1, len(rows[-1]))])
+    return sum(rows[i][-1] for i in range(len(rows) - 2, -1, -1))
+
+
+lines = get_data().splitlines()
+print(sum(f(list(map(int, s.split()))) for s in lines))
+print(sum(f(list(map(int, s.split()))[::-1]) for s in lines))
