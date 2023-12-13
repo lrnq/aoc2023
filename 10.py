3b049b988ec804a95e3f0511a240e7a51a9662f8
lrnq <26392224+lrnq@users.noreply.github.com>
2023-12-13 10:45:12 +0100

    add 10

--- 10.py
+++ 10.py
@@ -8,24 +8,24 @@ n = len(data)
 G = defaultdict(set)
 
 directions = {
-    "|": {"up", "down"},
-    "-": {"left", "right"},
-    "7": {"down", "left"},
-    "F": {"down", "right"},
-    "J": {"up", "left"},
-    "L": {"up", "right"},
-    "S": {"up", "down", "left", "right"},
+    "|": {"U", "D"},
+    "-": {"L", "R"},
+    "7": {"D", "L"},
+    "F": {"D", "R"},
+    "J": {"U", "L"},
+    "L": {"U", "R"},
+    "S": {"U", "D", "L", "R"},
 }
 
 
 def add_edge(i, j, dir):
-    if dir == "up" and i - 1 > -1 and data[i - 1][j] in "|F7":
+    if dir == "U" and i - 1 > -1 and data[i - 1][j] in "|F7":
         G[(i, j)].add((i - 1, j))
-    elif dir == "down" and i + 1 < n and data[i + 1][j] in "|LJ":
+    elif dir == "D" and i + 1 < n and data[i + 1][j] in "|LJ":
         G[(i, j)].add((i + 1, j))
-    elif dir == "left" and j - 1 >= 0 and data[i][j - 1] in "-LF":
+    elif dir == "L" and j - 1 >= 0 and data[i][j - 1] in "-LF":
         G[(i, j)].add((i, j - 1))
-    elif dir == "right" and j + 1 < n and data[i][j + 1] in "-J7":
+    elif dir == "R" and j + 1 < n and data[i][j + 1] in "-J7":
         G[(i, j)].add((i, j + 1))
 
 
@@ -39,6 +39,5 @@ for i in range(n):
 cycle = nx.find_cycle(nx.Graph(G), source=src)
 print("Part 1", len(cycle) // 2)
 # This is overkill. Since the polygon is rectilinear we can just shoot a ray in either orthogonal direction and count the number of tiles
-# we cross that are orthogonal to the direction of the ray.
-polygon = Polygon([u for (u, v) in cycle])
-print("Part 2:", sum(Point(i, j).within(polygon) for (i, j) in all_tiles))
+# we cross that are orthogonal to the direction of the ray. This would also work for simple polygons, but it requires more work.
+print("Part 2:", sum(Point(i, j).within(Polygon([u for (u, v) in cycle])) for i in range(n) for j in range(n)))
