920ad27988dd02a07deb7a8e4c742295fe82f1c2
lrnq <26392224+lrnq@users.noreply.github.com>
2023-12-18 17:57:44 +0100

    clean up

--- 18.py
+++ 18.py
@@ -12,16 +12,15 @@ step_func = {
     "D": lambda x, y, s: (x + s, y),
 }
 for part2 in 0, 1:
-    cur_pos = (0, 0)
+    x, y = (0, 0)
     edges = [(0, 0)]
     for line in data:
-        x, y = cur_pos
         direction, step_size, hex_code = line.split()
         if part2:
             step_size = int(hex_code[2:-2], 16)
             direction = i2d[hex_code[-2]]
-        cur_pos = step_func[direction](x, y, int(step_size))
-        edges.append(cur_pos)
+        x, y = step_func[direction](x, y, int(step_size))
+        edges.append((x, y))
 
     polygon = geometry.Polygon(edges)
     print(f"Part {part2+1}", (polygon.area + 1) - (polygon.length // 2) + polygon.length)
